62561f59538a9e5421bf4c96325d2122e2c10caf
James Bennett <2009bennettj@gmail.com>
2022-10-29 14:22:01 +0200

    Inclusion of the bottom navigation bar

---
+++ lib/root_scaffold.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:pokemon_demo/home/home_page.dart';
+
+class RootScaffold extends StatefulWidget {
+  const RootScaffold({super.key});
+
+  @override
+  State<RootScaffold> createState() => _RootScaffoldState();
+}
+
+class _RootScaffoldState extends State<RootScaffold> {
+  int _selectedIndex = 0;
+  static const List<Widget> _pageWidgets = <Widget>[
+    HomePage(),
+    Text('Index 1: Business'),
+    Text('Index 2: School'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pokémon Demo App'),
+      ),
+      body: Center(
+        child: _pageWidgets.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
